c3b3746516227a93974d0fabfc7dc4d1beb51127
Bernardo <bemeine15@hotmail.com>
2023-08-22 18:38:14 -0300

    Project created

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Section12ChessGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
