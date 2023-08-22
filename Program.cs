b7e1ed0b2f5b6f88c517ada24045c36966c9a5c0
Bernardo <bemeine15@hotmail.com>
2023-08-22 19:03:01 -0300

    Created folder Entities, Board and the Position Class

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Section12ChessGame
+﻿using Section12ChessGame.Entities.Board;
+
+namespace Section12ChessGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Position p = new(3, 4);
+
+            Console.WriteLine(p);
         }
     }
 }
