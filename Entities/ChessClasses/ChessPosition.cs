ac00178973e181aaa409787757c4b4b4acf9f189
Bernardo <bemeine15@hotmail.com>
2023-08-23 10:58:36 -0300

    Created class ChessPosition

---
+++ Entities/ChessClasses/ChessPosition.cs
@@ -0,0 +1,31 @@
+﻿using Section12ChessGame.Entities.BoardClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section12ChessGame.Entities.ChessClasses
+{
+    internal class ChessPosition
+    {
+        public char Row { get; set; }
+        public int Column { get; set; }
+
+        public ChessPosition(char row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Position ConvertToPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return $"{Column}{Row}";
+        }
+    }
+}
